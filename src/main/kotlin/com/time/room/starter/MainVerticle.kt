f6dcc375964ca7245793b1dba6dd964f1812a2c8
João Gabriel Bracaioli <joaobracaioli@gmail.com>
2021-05-19 22:46:28 -0300

    Add route exemple

--- src/main/kotlin/com/time/room/starter/MainVerticle.kt
+++ src/main/kotlin/com/time/room/starter/MainVerticle.kt
@@ -1,18 +1,22 @@
 package com.time.room.starter
 
 import io.vertx.core.AbstractVerticle
+import io.vertx.core.Future
 import io.vertx.core.Promise
+import io.vertx.core.json.JsonObject
+import io.vertx.ext.web.Router
 
 class MainVerticle : AbstractVerticle() {
 
   override fun start(startPromise: Promise<Void>) {
+    val route = Router.router(vertx)
+    route.route("/some/path") // this handler will ensure that the response is serialized to json
+      // the content type is set to "application/json"
+      .respond { ctx -> Future.succeededFuture(JsonObject().put("hello", "world"))}
+
     vertx
       .createHttpServer()
-      .requestHandler { req ->
-        req.response()
-          .putHeader("content-type", "text/plain")
-          .end("Hello from Vert.x!")
-      }
+      .requestHandler(route)
       .listen(8888) { http ->
         if (http.succeeded()) {
           startPromise.complete()
